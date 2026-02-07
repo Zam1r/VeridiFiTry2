5225a667fe29473c0b6eec09e78b35c87d4f84f1
Zam1r <zamirislamaj00@gmail.com>
2026-02-07 20:24:29 +0000

    add test helper functions

---
+++ test/helpers.ts
@@ -0,0 +1,20 @@
+/**
+ * Test helper functions
+ */
+
+import { expect } from "chai";
+
+export async function expectRevert(
+  promise: Promise<any>,
+  expectedError?: string
+) {
+  try {
+    await promise;
+    expect.fail("Expected transaction to revert");
+  } catch (error: any) {
+    if (expectedError) {
+      expect(error.message).to.include(expectedError);
+    }
+  }
+}
+
