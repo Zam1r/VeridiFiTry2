2d259a73694286f4a532540d0bafe71e89ee41a3
Zam1r <zamirislamaj00@gmail.com>
2026-02-07 20:24:31 +0000

    add plasma helper functions

---
+++ scripts/plasma/helpers.ts
@@ -0,0 +1,12 @@
+/**
+ * Helper functions for Plasma scripts
+ */
+
+export function validateAddress(address: string): boolean {
+  return /^0x[a-fA-F0-9]{40}$/.test(address);
+}
+
+export function formatAmount(amount: string): string {
+  return amount;
+}
+
