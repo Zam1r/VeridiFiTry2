20a65f34adef441bd57ea8a559defd09e139ed22
Zam1r <zamirislamaj00@gmail.com>
2026-02-07 20:24:28 +0000

    add utility helper functions

---
+++ scripts/utils/helpers.ts
@@ -0,0 +1,12 @@
+/**
+ * Utility helper functions for scripts
+ */
+
+export function formatAddress(address: string): string {
+  return `${address.slice(0, 6)}...${address.slice(-4)}`;
+}
+
+export function delay(ms: number): Promise<void> {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
